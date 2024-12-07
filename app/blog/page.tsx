915fea80afcacbab16a228b7f2550390a78c477b
kodychik <73432901+kodychik@users.noreply.github.com>
2024-12-07 14:44:06 -0500

    shush

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,14 +1,14 @@
 import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
+  title: 'Findings',
+  description: 'My Findings on state of the art image denoising techniques.',
 }
 
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Progression of Understanding</h1>
       <BlogPosts />
     </section>
   )
--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -5,11 +5,8 @@ const navItems = {
     name: 'home',
   },
   '/blog': {
-    name: 'blog',
-  },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy',
-  },
+    name: 'Findings',
+  }
 }
 
 export function Navbar() {
